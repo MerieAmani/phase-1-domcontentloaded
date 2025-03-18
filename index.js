583275314f85d357afd2c49a636e423bf5043742
matthew <meriematthew01@gmail.com>
2025-03-18 13:18:12 +0300

    adds DOM content loaded

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    const paragraph = document.getElementById("text");
+    paragraph.textContent = "This is really cool!";
+});
